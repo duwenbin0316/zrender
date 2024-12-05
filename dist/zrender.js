919136ce58a00cd2723a701f1ed61b430012a540
mskj-duwenbin <duwenbin@mskj.com>
2024-12-05 12:34:21 +0800

    build

--- dist/zrender.js
+++ dist/zrender.js
@@ -235,6 +235,7 @@ var nativeFilter = arrayProto.filter;
 var nativeSlice = arrayProto.slice;
 var nativeMap = arrayProto.map;
 var nativeReduce = arrayProto.reduce;
+var protoKey = '__proto__';
 
 // Avoid assign to an exported variable, for transforming to cjs.
 var methods = {};
@@ -297,7 +298,8 @@ function clone(source) {
     else if (!BUILTIN_OBJECT[typeStr] && !isPrimitive(source) && !isDom(source)) {
         result = {};
         for (var key in source) {
-            if (source.hasOwnProperty(key)) {
+            // Check if key is __proto__ to avoid prototype pollution
+            if (source.hasOwnProperty(key) && key !== protoKey) {
                 result[key] = clone(source[key]);
             }
         }
@@ -320,7 +322,8 @@ function merge(target, source, overwrite) {
     }
 
     for (var key in source) {
-        if (source.hasOwnProperty(key)) {
+        // Check if key is __proto__ to avoid prototype pollution
+        if (source.hasOwnProperty(key) && key !== protoKey) {
             var targetProp = target[key];
             var sourceProp = source[key];
 
@@ -369,7 +372,8 @@ function mergeAll(targetAndSources, overwrite) {
  */
 function extend(target, source) {
     for (var key in source) {
-        if (source.hasOwnProperty(key)) {
+        // Check if key is __proto__ to avoid prototype pollution
+        if (source.hasOwnProperty(key) && key !== protoKey) {
             target[key] = source[key];
         }
     }
@@ -385,6 +389,7 @@ function extend(target, source) {
 function defaults(target, source, overlay) {
     for (var key in source) {
         if (source.hasOwnProperty(key)
+            && key !== protoKey
             && (overlay ? source[key] != null : target[key] == null)
         ) {
             target[key] = source[key];
